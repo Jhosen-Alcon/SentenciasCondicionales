788fffdb8c108f16d156c15f579503487f2604d5
JhosenAlcon <aqj2022999@est.univalle.edu>
2023-06-28 21:34:51 -0400

    Agregar archivos de proyecto.

---
+++ SentenciasCondicionales/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SentenciasCondicionales
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Valores lógicos
+            //bool valorBooleano = (0!=5) && (8<3);
+            //bool valorBooleano = (0 != 5) || (8 < 3);
+            bool valorBooleano = !(0 == 5);
+            Console.WriteLine(valorBooleano);
+            Console.ReadKey();
+
+
+            //Sentencias condicional IF
+            int num1 = 11;
+            int num2 = 12;
+            if (num1 > num2)
+            {
+                Console.WriteLine("El numero" + num1 + "es mayor que " + num2);
+            }
+            else
+            {
+                Console.WriteLine("El numero" + num2 + "es mayor que " + num1);
+            }
+
+
+            //Sentencia condicional switch
+            int mes = 1;
+            switch (mes)
+            {
+                case 1: Console.WriteLine("El primer mes es enero");
+                    break;
+                case 2: Console.WriteLine("El segundo mes es febrero");
+                    break;
+                default: Console.WriteLine("Mes incorrecto");
+                    break;
+            }
+        }
+    }
+}
